0659f7d0ec8aff21df690df168239b9ea294c56c
Luqman Tuke <lsuleiman2002@gmail.com>
2023-05-12 11:06:55 +0300

    bannerImage and bannerText responsive tweak

--- src/components/banner/BannerApp.jsx
+++ src/components/banner/BannerApp.jsx
@@ -4,11 +4,11 @@ import { BannerOne } from '../../utilities/assetIndex'
 
 const BannerApp = () => {
   return (
-   <Box backgroundColor='brand.purpleOne' boxSize='container.md'  width='100%' pl={10} >
-<Flex   alignItems={{md:'center',lg:'start',sm:'center',base:'center'}}direction={{md:'column',lg:'row',sm:'column',base:'column'}} >
+   <Box backgroundColor='brand.purpleOne'   width='100%'  >
+<Flex  direction={{md:'column',lg:'row',sm:'column',base:'column'}} >
 
     {/* First box to display banner text */}
-    <Box flex={1} flexGrow={1} pt={20}>
+    <Box flex={1} flexGrow={1} pt={20} pl={10} >
       <Flex direction='column' >
         <Heading color='brand.blackOne' as='h1' fontSize={50}>Revolutionize<br /> school timetabling<br/> with DevHeroes.</Heading>
         <Text pt={7} color='brand.blackTwo' fontSize={20}>
@@ -18,7 +18,7 @@ const BannerApp = () => {
     </Box>
 
     {/* Second Box to display banner image */}
-    <Box width={{md:'100%',sm:'100%',base:'100%'}}    flex={1} flexGrow={1}>
+    <Box width={{md:'100%',sm:'100%',base:'100%'}} pt={{md:'10',sm:'10',base:'10', lg:'0'}} height='100%'    flex={1} flexGrow={1}>
       <Image    src={BannerOne} />
     </Box>
 </Flex>

559c4067e557c8f6b5b58aea8d34045523d0041f
Luqman Tuke <lsuleiman2002@gmail.com>
2023-05-19 14:35:32 +0300

    modified asset

--- src/utilities/assetIndex.js
+++ src/utilities/assetIndex.js
@@ -1,4 +1,4 @@
-export  {default as BannerOne} from '../assets/image1.jpeg'
+export  {default as BannerOne} from '../assets/schoolTimetable.jpg'
 export  {default as Planning} from '../assets/planning.png'
 export  {default as TimeTableCreation} from '../assets/timetable_creation.png'
 export  {default as WebDevelopment} from '../assets/webdevelopment.png'

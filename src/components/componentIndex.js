f07cb8d0d6f6f21ca933ea992080b19f1895f6c2
Luqman Tuke <lsuleiman2002@gmail.com>
2023-05-27 09:14:53 +0300

    implemented register school

--- src/components/componentIndex.js
+++ src/components/componentIndex.js
@@ -3,4 +3,5 @@ export {default as WhyChooseUs} from './whychooseus/WhyChooseUs';
 export {default as Testimonial} from './testimonial/Testimonial';
 export {default as WhatWeDo} from './whatwedo/WhatWeDo';
 export {default as Footer} from './footer/Footer';
+export {default as RegisterSchool} from './registration/RegistrationSchool';
 export {default as TimetableTemplate} from './timetableTemplate/TimetableTemplate';
---
+++ src/components/registration/RegistrationSchool.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+import Iframe from 'react-iframe'
+
+
+const RegistrationSchool = () => {
+  return (
+    <div>
+<Iframe url="https://airtable.com/embed/shrt48PZwJGR6Hyqi?backgroundColor=red"
+        width="100%"
+        height="733px"
+
+        id=""
+        className="airtable-embed"
+  />
+    </div>
+  )
+}
+
+export default RegistrationSchool
